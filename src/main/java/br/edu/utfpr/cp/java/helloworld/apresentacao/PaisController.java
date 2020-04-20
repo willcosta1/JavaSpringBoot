8502274be0e6dcf0ec90807c1704ea28e125c9fd
Willian Costa <williancosta.amaral@gmail.com>
2020-04-20 11:53:32 -0300

    Implementado persistencia utilizando-se de JPA com H2(em memoria)

--- src/main/java/br/edu/utfpr/cp/java/helloworld/apresentacao/PaisController.java
+++ src/main/java/br/edu/utfpr/cp/java/helloworld/apresentacao/PaisController.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,22 +14,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 
 @Controller
 @Log
+@RequiredArgsConstructor
 public class PaisController {
-    private List<PaisModel> listaPaises;
+    @Autowired
+    private final PaisRepository paisRepository;
 
-    public PaisController(){
-        listaPaises = Stream.of(
-            PaisModel.builder().id(1L).nome("Brasil").sigla("BR").build(),
-            PaisModel.builder().id(2L).nome("Estados Unidos").sigla("EUA").build()
-        ).collect(Collectors.toList());
+    /* Lombok com @RequiredArgsConstructor está realizando a mesma função
+    public PaisController(PaisRepository paisRepository){
+        this.paisRepository = paisRepository;
     }
+    */
 
     private void adicionaListaPaisesNaMemoria(Model memoria){
-        memoria.addAttribute("listaPaises", this.listaPaises);
+        memoria.addAttribute("listaPaises", paisRepository.findAll());
     }
 
     @GetMapping ("/pais")
@@ -38,7 +41,7 @@ public class PaisController {
     }
     @GetMapping ("/pais/apagar")
     public String apagar(@RequestParam Long id){
-        listaPaises.removeIf(paisAtual -> paisAtual.getId().equals(id));
+        paisRepository.deleteById(id);
 
         return "redirect:/pais";
     }
@@ -53,14 +56,13 @@ public class PaisController {
             return "pais-page";
         }
 
-        pais.setId(new Long(listaPaises.size() + 1));
-        listaPaises.add(pais);
+        paisRepository.save(pais);
 
         return "redirect:/pais";
     }
     @GetMapping("/pais/preparaAlterar")
     public String preparaAlterar(@RequestParam Long id, Model memoria){
-        var pais = listaPaises.stream().filter(paisAtual -> paisAtual.getId().equals(id)).findAny().get();
+        var pais = paisRepository.findById(id).get();
         memoria.addAttribute("paisAtual", pais);
         this.adicionaListaPaisesNaMemoria(memoria);
         memoria.addAttribute("alterar", true);
@@ -70,10 +72,12 @@ public class PaisController {
     @PostMapping("/pais/alterar")
     public String alterar(PaisModel paisNovo){
 
-        var pais = listaPaises.stream().filter(paisAtual -> paisAtual.getId().equals(paisNovo.getId())).findAny().get();
+        var pais = paisRepository.findById(paisNovo.getId()).get();
 
         pais.setNome(paisNovo.getNome());
         pais.setSigla(paisNovo.getSigla());
+
+        paisRepository.saveAndFlush(pais);
 
         return "redirect:/pais";
     }
--- src/main/java/br/edu/utfpr/cp/java/helloworld/apresentacao/PaisModel.java
+++ src/main/java/br/edu/utfpr/cp/java/helloworld/apresentacao/PaisModel.java
@@ -1,16 +1,22 @@
 package br.edu.utfpr.cp.java.helloworld.apresentacao;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @Builder
+@Entity
+@NoArgsConstructor //Gerar um construtor sem argumentos automaticamente
 public class PaisModel {
-    
+    @Id @GeneratedValue
     private Long id;
     @Size (min = 3, max = 15, message = "O nome deve ter entre 3 e 15 caracteres")
     private String nome;
